b280efc1d1acd04659f56d1b6cc9f5ef16729951
Aryanmahakur <mahakuraryan@gmail.com>
2025-07-05 23:08:52 +0530

    uf

--- doublylinkedlist.cpp
+++ doublylinkedlist.cpp
@@ -2,57 +2,78 @@
 using namespace std;
 
 // Node definition
-class Node {
+class Node
+{
 public:
     int data;
-    Node* next;
-    Node* prev;
+    Node *next;
+    Node *prev;
 
-    Node(int val) {
+    Node(int val)
+    {
         data = val;
         next = nullptr;
         prev = nullptr;
     }
 };
 
-// Insert at tail
-void insertAtTail(Node*& head, int val) {
-    Node* newNode = new Node(val);
-
-    if (head == nullptr) {
-        head = newNode;
-        return;
-    }
-
-    Node* temp = head;
-    while (temp->next != nullptr) {
-        temp = temp->next;
-    }
-
-    temp->next = newNode;
-    newNode->prev = temp;
-}
-
-// Print list forward
-void printList(Node* head) {
-    Node* temp = head;
-    cout << "Doubly Linked List (Forward): ";
-    while (temp != nullptr) {
+// Function to print the doubly linked list
+void printList(Node *head)
+{
+    Node *temp = head;
+    cout << "Doubly Linked List: ";
+    while (temp != nullptr)
+    {
         cout << temp->data << " <-> ";
         temp = temp->next;
     }
     cout << "NULL" << endl;
+};
+void insertattail(Node*& head, int val)
+{
+    Node *newnode = new Node(val);
+    if (head == nullptr)
+    {
+        head = newnode;
+        return;
+    }
+    Node *temp = head;
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+        /* code */
+    }
+    temp->next = newnode;
+    newnode->prev = temp;
+  //  head=newnode;
+};
+void deleteFirst(Node*& head) {
+    if (head == nullptr) return;
+
+    Node* temp = head;
+    head = head->next;
+
+    if (head != nullptr)
+        head->prev = nullptr;
+
+    delete temp;
 }
 
-int main() {
-    Node* head = nullptr;
+int main()
+{
+    // Create nodes manually
+    Node *head = new Node(10);
+    Node *second = new Node(20);
+    Node *third = new Node(30);
 
-    // Inserting some values
-    insertAtTail(head, 10);
-    insertAtTail(head, 20);
-    insertAtTail(head, 30);
-    insertAtTail(head, 40);
+    // Linking nodes
+    head->next = second;
+    second->prev = head;
 
+    second->next = third;
+    third->prev = second;
+    deleteFirst(head);
+    insertattail(head,100);
     // Print the list
     printList(head);
 
