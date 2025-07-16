12cbd82308865c9f283db6aa4353fc21d4eb9cff
Aryanmahakur <mahakuraryan@gmail.com>
2025-07-16 23:30:59 +0530

    uf

--- graph.cpp
+++ graph.cpp
@@ -181,7 +181,7 @@ int main() {
         int u, v;
         cin >> u >> v;
 
-        // Undirected edgea
+        // Undirected edge
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
